831eff018e6b92119c2d127ca48e32def73535cd
MarceloJoia <sitejoia@hotmail.com>
2022-04-27 11:59:33 -0300

    Classes

---
+++ recortar-string-split/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String s = "potato apple lemon";
+		
+		String[] vet = s.split(" ");
+		
+		System.out.println(vet[0]);
+		System.out.println(vet[1]);
+		System.out.println(vet[2]);
+	}
+
+}
