52d8b805937883a5087c4ae0fced464b5c4d656b
MarceloJoia <sitejoia@hotmail.com>
2022-05-28 17:46:44 -0300

    Trabalhando com construtores

---
+++ construtores/src/programa/construtor.java
@@ -0,0 +1,54 @@
+package programa;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class construtor {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		//Product product = new Product();
+			//System.out.println(product.name);
+			//System.out.println(product.price);
+			//System.out.println(product.quantity);
+
+		System.out.println("Enter product data");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		Integer quantity = sc.nextInt();
+
+		Product product = new Product(name, price, quantity);
+		
+		System.out.println();
+
+		System.out.println("Product data: " + product);
+		System.out.println();
+
+		System.out.print("Enter the number of products to be added in stock: ");
+		quantity = sc.nextInt();
+
+		product.addProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+
+		System.out.println();
+
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+
+		System.out.println();
+		System.out.println("Updated data: " + product);
+
+		sc.close();
+
+	}
+
+}
