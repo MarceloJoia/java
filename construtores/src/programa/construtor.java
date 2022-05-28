6fb2efbb7b724a188c88e3fe0541c795a45c72db
MarceloJoia <sitejoia@hotmail.com>
2022-05-28 18:30:10 -0300

    Construtor com sobre carga

--- construtores/src/programa/construtor.java
+++ construtores/src/programa/construtor.java
@@ -15,16 +15,16 @@ public class construtor {
 			//System.out.println(product.name);
 			//System.out.println(product.price);
 			//System.out.println(product.quantity);
+		
+		//Product product = new Product();
 
 		System.out.println("Enter product data");
 		System.out.print("Name: ");
 		String name = sc.nextLine();
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
-		System.out.print("Quantity in stock: ");
-		Integer quantity = sc.nextInt();
-
-		Product product = new Product(name, price, quantity);
+		
+		Product product = new Product(name, price);
 		
 		System.out.println();
 
@@ -32,7 +32,7 @@ public class construtor {
 		System.out.println();
 
 		System.out.print("Enter the number of products to be added in stock: ");
-		quantity = sc.nextInt();
+		int quantity = sc.nextInt();
 
 		product.addProducts(quantity);
 		System.out.println();
