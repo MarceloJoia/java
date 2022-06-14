f7f7d979ecb62bf933222d26c8348464e2b9c529
MarceloJoia <sitejoia@hotmail.com>
2022-06-14 11:37:27 -0300

    Total de todas as contas, Update saldo de todas as contas

--- 014_Classes_Abstratas_heranca_total/src/appication/Main.java
+++ 014_Classes_Abstratas_heranca_total/src/appication/Main.java
@@ -1,5 +1,9 @@
 package appication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
@@ -7,11 +11,29 @@ import entities.SavingsAccount;
 public class Main {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 
-		Account acc1 = new Account(1001, "Alex", 1000.0);
-		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.00);
+		// • Totalizar o saldo de todas as contas.
+		List<Account> list = new ArrayList<Account>();
+		list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+		list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+		list.add(new SavingsAccount(1003, "Bob", 300.0, 0.01));
+		list.add(new BusinessAccount(1002, "Ana", 500.0, 500.0));
 
+		double soma = 0;
+		for (Account acc : list) {
+			soma += acc.getBalance();
+		}
+		System.out.printf("Total balance %.2f%n", soma);
+		System.out.println();
+		
+		
+		// • Depositar 10.00 em todas as contas.
+		for(Account acc : list) {
+			acc.deposit(10.0);
+		}
+		for(Account acc : list) {
+			System.out.printf("Update balance %d: %.2f%n", acc.getNumber(), acc.getBalance());
+		}
 	}
-
 }
