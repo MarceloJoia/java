be217c128893af3e8b9289e385c5c9810b0008bf
MarceloJoia <sitejoia@hotmail.com>
2022-07-04 19:03:06 -0300

    FileWriter e BufferedWriter

--- 017_3_FileWriter_e_BufferedWriter/src/application/Main.java
+++ 017_3_FileWriter_e_BufferedWriter/src/application/Main.java
@@ -1,11 +1,26 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
 
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+
+		String path = "C:\\ws-sts\\Java\\017_3_FileWriter_e_BufferedWriter\\file.txt";
+
+		try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path, true))) {
+			
+			for (String line : lines) {
+				bufferedWriter.write(line);
+				bufferedWriter.newLine();//FAZ UMA QUEBRA DE LINHA
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
